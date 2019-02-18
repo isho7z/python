89dbe6eae2516976b19b378e9876946987286efb
isho7z <47661444+isho7z@users.noreply.github.com>
2019-02-18 09:19:08 -0300

    Add files via upload

---
+++ ExercicioCPF.py
@@ -0,0 +1,21 @@
+cpf=1234
+print(cpf)
+print('O meu cpf é',cpf)
+print(type(cpf))
+
+nome='Jose'
+telefone=40028922
+cpf=8855412477
+altura=1.80
+print(nome)
+print(telefone)
+print(cpf)
+
+print('O meu nome é',nome)
+print('Meu telofe é',telefone)
+print('Meu cpf é',cpf)
+print('Minha altura é',altura)
+print(type(nome))
+print(type(telefone))
+print(type(cpf))
+print(type(altura))
